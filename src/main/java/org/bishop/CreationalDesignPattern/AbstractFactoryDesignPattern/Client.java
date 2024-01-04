1106ed4c1b875c27caaabc3c3762c03232e11041
Bishop Bishal <bishop.bishal@blackngreen.com>
2024-01-04 13:50:05 +0530

    Understood the Abstract Factory Design Pattern with proper Example

--- src/main/java/org/bishop/CreationalDesignPattern/AbstractFactoryDesignPattern/Client.java
+++ src/main/java/org/bishop/CreationalDesignPattern/AbstractFactoryDesignPattern/Client.java
@@ -10,7 +10,7 @@ public class Client {
         * This Abstract Factory Design is more like Factory Design pattern but still there is some
         * things differ from Factory Design pattern
         * 1) It adds an abstract layer at Employee Factory .
-        * 2) It adds an Employee Abstract Factory which is  factory of factories.
+        * 2) It provide a concept of  factory of factories.
         * */
         Employee androidEmployee = EmployeeFactory.getEmployee(AndroidDeveloper::new);
         Employee webEmployee = EmployeeFactory.getEmployee(WebDeveloper::new);
